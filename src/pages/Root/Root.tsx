d4b21a53de14f9ec28934a6ff615e3dce56064cc
unique0902 <rla0591@naver.com>
2023-12-29 04:01:28 +0900

    Feat: add module css

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Root.module.css';
 
 const Root = () => {
-  return <div>root</div>;
+  return <div className={styles.wrapper}>root</div>;
 };
 
 export default Root;

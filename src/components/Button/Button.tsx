ca78a0747532dc7619f3e85346ae80ae3cf00200
unique0902 <rla0591@naver.com>
2024-01-01 16:55:30 +0900

    Feat: add button args setting

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import styles from './Button.module.css';
 import '../../styles/colorVars.css';
+
+type Color =
+  | 'main-purple'
+  | 'black'
+  | 'white'
+  | 'navy'
+  | 'grey'
+  | 'sub-gradation';
 interface Props {
-  /**
-   * 주요 색상
-   */
-  primary?: boolean;
   /**
    * 배경색
    */
-  backgroundColor?: 'mainPurple' | 'black';
+  backgroundColor?: Color;
+  /**
+   * 글자색
+   */
+  textColor?: Color;
   /**
    * 사이즈 설정
    */
-  size?: 'medium' | 'full';
+  size?: 'small' | 'medium' | 'full';
+  /**
+   * Border Radius 사이즈 설정
+   */
+  borderRadius?: 'medium' | 'large';
+  /**
+   * Border 색깔, 값 존재하지않을시 border none 처리됨
+   */
+  borderColor?: Color;
+  /**
+   * Border 두께, px기준이며 color와 함께 존재하지 않을시 border none처리됨
+   */
+  border?: number;
   /**
    * 버튼 속 텍스트
    */
   label: string;
+  /**
+   * 클릭 가능한지 상태
+   */
+  disabled: boolean;
+  /**
+   * 클릭 불가능시 배경 색깔
+   */
+  disabledBackgroundColor: Color;
+  /**
+   * 클릭 불가능시 글자 색깔
+   */
+  disabledTextColor: Color;
+  /**
+   * 클릭 불가능시 border 색깔
+   */
+  disabledBorderColor: Color;
   /**
    * 클릭 핸들러
    */
@@ -28,10 +64,17 @@ interface Props {
  * 버튼
  */
 const Button: React.FC<Props> = ({
-  primary = false,
   size = 'medium',
-  backgroundColor,
+  backgroundColor = 'main-purple',
+  textColor = 'white',
   label,
+  borderColor,
+  border,
+  disabled = false,
+  disabledBackgroundColor = 'grey',
+  disabledTextColor = 'white',
+  disabledBorderColor = 'grey',
+  borderRadius = 'medium',
   onClick,
 }: Props) => {
   let btnClassNames = `${styles.btn}`;
@@ -42,13 +85,40 @@ const Button: React.FC<Props> = ({
     case 'medium':
       btnClassNames += ` ${styles.btnMedium}`;
       break;
+    case 'small':
+      btnClassNames += ` ${styles.btnSmall}`;
+      break;
     default:
       throw new Error('not valid btn size!');
   }
+  switch (borderRadius) {
+    case 'medium':
+      btnClassNames += ` ${styles.roundedMedium}`;
+      break;
+    case 'large':
+      btnClassNames += ` ${styles.roundedLarge}`;
+      break;
+    default:
+      throw new Error('not valid borderRadius size!');
+  }
+
   return (
     <button
-      style={{ backgroundColor: 'var(--color-main-purple)' }}
+      style={{
+        backgroundColor: `var(--color-${
+          disabled ? disabledBackgroundColor : backgroundColor
+        })`,
+        color: `var(--color-${disabled ? disabledTextColor : textColor})`,
+        border:
+          borderColor && border
+            ? `${border}px solid var(--color-${
+                disabled ? disabledBorderColor : borderColor
+              })`
+            : 'none',
+      }}
       className={btnClassNames}
+      onClick={onClick}
+      disabled={disabled}
     >
       {label}
     </button>
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,9 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { Button } from './Button';
+import Button from '../components/Button/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
@@ -13,46 +12,46 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} satisfies Meta<typeof Button>;
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+
+export const Medium: Story = {
   args: {
-    primary: true,
+    size: 'medium',
     label: 'Button',
+    backgroundColor: 'main-purple',
+    textColor: 'white',
+    borderRadius: 'medium',
+    border: undefined,
+    borderColor: undefined,
+    disabled: false,
+    disabledBackgroundColor: 'grey',
+    disabledTextColor: 'white',
+    disabledBorderColor: 'grey',
+    onClick: () => {},
   },
 };
 
-export const Secondary: Story = {
+export const Full: Story = {
   args: {
+    size: 'full',
     label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
-
-export const Warning: Story = {
-  args: {
-    primary: true,
-    label: 'Delete now',
-    backgroundColor: 'red',
+    backgroundColor: 'main-purple',
+    textColor: 'white',
+    borderRadius: 'medium',
+    border: undefined,
+    borderColor: undefined,
+    disabled: false,
+    disabledBackgroundColor: 'grey',
+    disabledTextColor: 'white',
+    disabledBorderColor: 'grey',
+    onClick: () => {},
   },
 };

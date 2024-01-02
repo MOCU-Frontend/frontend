d5824bacd2b855af38f1d824098ff9faf20ad699
unique0902 <rla0591@naver.com>
2024-01-03 03:25:38 +0900

    Feat: make modal component

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Modal from '../../components/Modal/Modal';
 import styles from './Root.module.css';
 
 const Root = () => {
-  return <div className={styles.wrapper}>root</div>;
+  return (
+    <div className={styles.wrapper}>
+      root
+      <Modal />
+    </div>
+  );
 };
 
 export default Root;

97f467ad58174e5b8a656fd93101ba66315cb31e
HS <hasang2691@naver.com>
2024-01-04 23:56:45 +0900

    feat: solve conflict in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,27 +4,11 @@ declare module '*.css' {
 }
 
 declare module '*.svg' {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 2ebab96ccf9db279f99120105954884d59ccaaed
     import React = require('react');
   
     export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
     const src: string;
     export default src;
   }
-<<<<<<< HEAD
 
-  
-=======
-  import React = require('react');
 
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
->>>>>>> dev
-=======
->>>>>>> 2ebab96ccf9db279f99120105954884d59ccaaed

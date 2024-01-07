6165d8488b5e831229650ac086a8dee7dc00f8d7
unique0902 <rla0591@naver.com>
2024-01-07 20:37:59 +0900

    Feat: add script hook

---
+++ src/hooks/useScript.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+export function useScript(src: string) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<ErrorEvent | undefined>();
+
+  useEffect(() => {
+    let script: HTMLScriptElement | null = document.querySelector(
+      `script[src="${src}"]`
+    );
+
+    if (!script) {
+      script = document.createElement('script');
+      script.src = src;
+      script.async = true;
+    }
+
+    const handleLoad = () => setLoading(false);
+    const handleError = (error: ErrorEvent) => setError(error);
+
+    script.addEventListener('load', handleLoad);
+    script.addEventListener('error', handleError);
+
+    document.body.appendChild(script);
+
+    return () => {
+      if (script) {
+        script.removeEventListener('load', handleLoad);
+        script.removeEventListener('error', handleError);
+      }
+    };
+  }, [src]);
+
+  return [loading, error];
+}

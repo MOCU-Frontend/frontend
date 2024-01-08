73cd02a89b6322bf563d7482766b8c7403188f31
HS <hasang2691@naver.com>
2024-01-09 00:25:35 +0900

    Feat: add CheckFilterWithXBtn Component

---
+++ src/components/CheckFilter/CheckFilterWithXBtn/CheckFilterWithXBtn.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import styles from './CheckFilterSelect.module.css';
+import { ReactComponent as UndoCenterRegular } from '../../../assets/icon/undoCenterRegular.svg';
+import { colors } from '../../../styles/colors';
+type Color =
+  | 'main-purple'
+  | 'black'
+  | 'white'
+  | 'navy'
+  | 'grey'
+  | 'sub-gradation'
+  | 'bg-purple-light'
+  | 'sub-purple-light';
+
+interface Props {
+  /**
+   * 필터 속 텍스트
+   */
+  label: string;
+  /**
+   * 체크 상태 여부
+   */
+  isChecked: boolean;
+  /**
+   * 배경색
+   */
+  backgroundColor?: Color;
+  /**
+   * 글자색
+   */
+  textColor?: Color;
+  /**
+   * 체크되었을때의 배경색
+   */
+  checkedBackgroundColor?: Color;
+  /**
+   * 체크되었을때의 글자색
+   */
+  checkedTextColor?: Color;
+  /**
+   * 사이즈 설정
+   */
+  size?: 'small' | 'medium' | 'full';
+  /**
+   * arrow down icon 색깔
+   */
+  arrowColor?: string;
+  /**
+   * Border Radius 사이즈 설정
+   */
+  borderRadius?: 'medium' | 'large';
+  /**
+   * Border 색깔, 값 존재하지않을시 border none 처리됨
+   */
+  borderColor?: Color;
+  /**
+   * Border 두께, px기준이며 color와 함께 존재하지 않을시 border none처리됨
+   */
+  border?: number;
+
+  /**
+   * 컨텐트 클릭 핸들러
+   */
+  onClickContent: () => void;
+
+  /**
+   *  x 버튼 클릭 핸들러
+   */
+  onClickXBtn: () => void;
+}
+
+const CheckFilterSelect: React.FC<Props> = ({
+  label,
+  isChecked = false,
+  backgroundColor = 'bg-purple-light',
+  textColor = 'navy',
+  checkedBackgroundColor = 'main-purple',
+  checkedTextColor = 'white',
+  size = 'small',
+  arrowColor = colors.subPurpleLight,
+  borderRadius = 'large',
+  borderColor,
+  border,
+  onClickContent = () => {},
+  onClickXBtn = () => {},
+}: Props) => {
+  let btnClassNames = `${styles.btn}`;
+  switch (size) {
+    case 'full':
+      btnClassNames += ` ${styles.btnFull}`;
+      break;
+    case 'medium':
+      btnClassNames += ` ${styles.btnMedium}`;
+      break;
+    case 'small':
+      btnClassNames += ` ${styles.btnSmall}`;
+      break;
+    default:
+      throw new Error('not valid btn size!');
+  }
+  switch (borderRadius) {
+    case 'medium':
+      btnClassNames += ` ${styles.roundedMedium}`;
+      break;
+    case 'large':
+      btnClassNames += ` ${styles.roundedLarge}`;
+      break;
+    default:
+      throw new Error('not valid borderRadius size!');
+  }
+  return (
+    <div>
+        <button
+            className={btnClassNames}
+            style={{
+                backgroundColor: `var(--color-${
+                isChecked ? checkedBackgroundColor : backgroundColor
+                })`,
+                color: `var(--color-${isChecked ? checkedTextColor : textColor})`,
+                border:
+                borderColor && border
+                    ? `${border}px solid var(--color-${borderColor})`
+                    : 'none',
+            }}
+            >
+            {label}
+            <UndoCenterRegular width={24} height={24} stroke={arrowColor} />
+        </button>
+    </div>
+    
+  );
+};
+
+export default CheckFilterSelect;
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -5,6 +5,7 @@ import styles from './Search.module.css';
 
 import HeaderBackBtn from '../../components/HeaderBackBtn/HeaderBackBtn';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import Button from '../../components/Button/Button';
 
 const Search = () => {
   return (
@@ -13,15 +14,21 @@ const Search = () => {
             <HeaderBackBtn
             headerPaddingSize='search'
             onClickBackBtn={() => {}}
-            />
-            <SearchBar 
+            >
+              <SearchBar 
                 placeholder='찾고 싶은 가게를 검색해 보세요'
                 onClickSearchBtn={() => {}}
             />
+            </HeaderBackBtn>
+            
         </div>
 
         <div className = {styles.content_search_recent}>
           <div className = {styles.search_title}>최근 검색어</div>
+          <Button
+            size = 'medium'
+            label = 'string'
+          />
         </div>
     </div>
   );

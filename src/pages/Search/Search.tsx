1bad8c17782d43671c89806b142e1a1ec655fbf8
HS <hasang2691@naver.com>
2024-01-09 01:04:19 +0900

    Feat: add recent search filter in Search

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -5,7 +5,7 @@ import styles from './Search.module.css';
 
 import HeaderBackBtn from '../../components/HeaderBackBtn/HeaderBackBtn';
 import SearchBar from '../../components/SearchBar/SearchBar';
-import Button from '../../components/Button/Button';
+import CheckFilterWithXBtn from './../../components/CheckFilter/CheckFilterWithXBtn/CheckFilterWithXBtn';
 
 const Search = () => {
   return (
@@ -25,10 +25,40 @@ const Search = () => {
 
         <div className = {styles.content_search_recent}>
           <div className = {styles.search_title}>최근 검색어</div>
-          <Button
-            size = 'medium'
-            label = 'string'
-          />
+          <div className = {styles.filtersWrapper}>
+            <CheckFilterWithXBtn
+              onClickContent = {() => {}}
+              onClickXBtn={() => {}}
+              isChecked = {false}
+              label = '베이글'
+            />
+            <CheckFilterWithXBtn
+              onClickContent = {() => {}}
+              onClickXBtn={() => {}}
+              isChecked = {false}
+              label = '스터디카페'
+            />
+            <CheckFilterWithXBtn
+              onClickContent = {() => {}}
+              onClickXBtn={() => {}}
+              isChecked = {false}
+              label = '도넛'
+            />
+            <CheckFilterWithXBtn
+              onClickContent = {() => {}}
+              onClickXBtn={() => {}}
+              isChecked = {false}
+              label = '대학로'
+            />
+            <CheckFilterWithXBtn
+              onClickContent = {() => {}}
+              onClickXBtn={() => {}}
+              isChecked = {false}
+              label = '건대입구'
+            />
+            
+          </div>
+          
         </div>
     </div>
   );

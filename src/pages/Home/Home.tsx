d9090b43d82da9d0b15fd7b113f93223da53cb16
unique0902 <rla0591@naver.com>
2024-01-09 19:58:01 +0900

    Feat: change Home MenuBtns

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,3 @@
-// Home.tsx
-
 import React from 'react';
 import styles from './Home.module.css';
 import { ReactComponent as MapIcon } from '../../assets/icon/map.svg';
@@ -8,15 +6,17 @@ import { ReactComponent as AlarmIcon } from '../../assets/icon/alarm.svg';
 import { ReactComponent as SettingIcon } from '../../assets/icon/setting.svg';
 import { ReactComponent as ProfileIcon } from '../../assets/icon/profile.svg';
 
+import { ReactComponent as ShoppingIcon } from '../../assets/icon/shopping.svg';
+import { ReactComponent as StampIcon } from '../../assets/icon/emptyStamp.svg';
+import { ReactComponent as PresentIcon } from '../../assets/icon/present.svg';
+import { ReactComponent as RocketIcon } from '../../assets/icon/rocket.svg';
+
 import BottomNavigation from '../../components/bottomNavigation/BottomNavigation';
-import { menuData } from '../../store/data/homeData';
-import MenuGridItem from '../../components/MenuGridItem/MenuGridItem'; // MenuGridItem 컴포넌트를 import합니다.
 import { useNavigate } from 'react-router-dom';
 import { colors } from '../../styles/colors';
 import HomeLocationSec from '../../components/Home/atoms/Location/HomeLocationSec';
-import HomeMenuBtnTitleText from '../../components/Home/atoms/Text/MenuBtnTitle/HomeMenuBtnTitleText';
-import HomeMenuBtnInformText from '../../components/Home/atoms/Text/MenuBtnInform/HomeMenuBtnInformText';
-import HomeMenuBtnSubText from '../../components/Home/atoms/Text/MenuBtnSub/HomeMenuBtnSubText';
+import HomeMenuFullBtn from '../../components/Home/atoms/Button/MenuFullBtn/HomeMenuFullBtn';
+import HomeMenuGridBtn from '../../components/Home/atoms/Button/MenuGridBtn/HomeMenuGridBtn';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -39,24 +39,38 @@ const Home = () => {
       </div>
 
       <div className={styles.main}>
-        <button className={styles.menuMapBtn} onClick={() => navigate('/map')}>
-          <div className={styles.menuMapDescriptionSec}>
-            <HomeMenuBtnTitleText text='지도' />
-            <HomeMenuBtnSubText text='내 근처에 있는 맛집 찾고 혜택도 얻을 수 있다?' />
-          </div>
-          <HomeMenuBtnInformText text='현위치 : 성북구 정릉로 77' />
-          <MapIcon
-            width={32}
-            height={32}
-            stroke={colors.subPurpleLight}
-            className={styles.mapIcon}
-          />
-        </button>
+        <HomeMenuFullBtn
+          onClick={() => navigate('/map')}
+          titleText='지도'
+          subText='내 근처에 있는 맛집 찾고 혜택도 얻을 수 있다?'
+          informText='현위치 : 성북구 정릉로 77'
+          Icon={MapIcon}
+        />
         <div className={styles.menu_grid_container}>
-          <MenuGridItem value={menuData[0]} onClick={() => {}} />
-          <MenuGridItem value={menuData[1]} onClick={() => {}} />
-          <MenuGridItem value={menuData[2]} onClick={() => {}} />
-          <MenuGridItem value={menuData[3]} onClick={() => {}} />
+          <HomeMenuGridBtn
+            titleText='가게'
+            subText='당신이 찾는 혜택 맛집은?'
+            Icon={ShoppingIcon}
+            onClick={() => {}}
+          />
+          <HomeMenuGridBtn
+            titleText='적립'
+            subText='쿠폰 적립 현황과 달성 혜택'
+            Icon={StampIcon}
+            onClick={() => {}}
+          />
+          <HomeMenuGridBtn
+            titleText='선물'
+            subText='선물하고 싶은 사람이 있나요?'
+            Icon={PresentIcon}
+            onClick={() => {}}
+          />
+          <HomeMenuGridBtn
+            titleText='미션'
+            subText='미션 완료하고 적립금 쌓기'
+            Icon={RocketIcon}
+            onClick={() => {}}
+          />
         </div>
 
         <div className={styles.content_event}></div>

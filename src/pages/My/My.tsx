92f4ae0adc237764b37853822e86dd5ee45b18bd
unique0902 <rla0591@naver.com>
2024-01-09 21:23:57 +0900

    Feat: change profile btn

--- src/pages/My/My.tsx
+++ src/pages/My/My.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HomeHeader from '../../components/Home/atoms/Header/HomeHeader';
 import styles from './My.module.css';
 import { ReactComponent as ProfileIcon } from '../../assets/icon/profile.svg';
-import HomeLocationSec from '../../components/Home/atoms/Location/HomeLocationSec';
+import { ReactComponent as ProfileGradationIcon } from '../../assets/icon/profileGradationBadgeMode.svg';
 import MyTopSection from '../../components/My/atoms/TopSection/MyTopSection';
 import HomeBottomNavigation from '../../components/Home/atoms/BottomNavigation/HomeBottomNavigation';
 
@@ -13,7 +13,9 @@ const My: React.FC<Props> = ({}: Props) => {
     <div className={styles.wholeWrapper}>
       <HomeHeader onClickAlarmBtn={() => {}} onClickSettingBtn={() => {}} />
       <div className={styles.myTopInformBar}>
-        <ProfileIcon width={48} height={48} />
+        <button className={styles.profileBtn} onClick={() => {}}>
+          <ProfileGradationIcon width={48} height={48} />
+        </button>
         <MyTopSection titleText='모쿠 님의 마이페이지' subText='@298370' />
       </div>
       <main className={styles.main}></main>

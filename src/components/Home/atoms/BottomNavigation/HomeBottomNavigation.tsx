435be82274fde372d0d260a03b2493d69bdeebd9
unique0902 <rla0591@naver.com>
2024-01-09 20:32:41 +0900

    Feat: refactor homeBottomNavigation

--- src/components/Home/atoms/BottomNavigation/HomeBottomNavigation.tsx
+++ src/components/Home/atoms/BottomNavigation/HomeBottomNavigation.tsx
@@ -3,22 +3,24 @@ import styles from './HomeBottomNavigation.module.css';
 import { ReactComponent as GradationHomeImage } from '../../../../assets/icon/gradationHome.svg';
 import { ReactComponent as CouponImage } from '../../../../assets/icon/coupon.svg';
 import { ReactComponent as MyImage } from '../../../../assets/icon/my.svg';
+import { colors } from '../../../../styles/colors';
+import HomeBottomNavigationText from '../Text/BottomNavigation/HomeBottomNavigationText';
 
 const HomeBottomNavigation = () => {
   return (
     <div className={styles.wholeWrapper}>
-      <div className={styles.navigationBar}>
-        <div className={styles.navibar_leftright}>
+      <div className={styles.btnSec}>
+        <div className={styles.normalBtn}>
           <GradationHomeImage width='24' height='24' />
-          <div className={styles.navibar_home_text}>홈</div>
+          <HomeBottomNavigationText text='홈' color={colors.mainPurple} />
         </div>
-        <div className={styles.navibar_button}>
-          <CouponImage fill='#FFFFFF' width='24' height='24' />
-          <div className={styles.navibar_button_text}>쿠폰</div>
+        <div className={styles.circleBtn}>
+          <CouponImage fill={colors.white} width='24' height='24' />
+          <HomeBottomNavigationText text='쿠폰' color={colors.white} />
         </div>
-        <div className={styles.navibar_leftright}>
-          <MyImage stroke='#CACACA' width='24' height='24' />
-          <div className={styles.navibar_my_text}>마이</div>
+        <div className={styles.normalBtn}>
+          <MyImage stroke={colors.greyLight02} width='24' height='24' />
+          <HomeBottomNavigationText text='마이' color={colors.greyLight02} />
         </div>
       </div>
     </div>

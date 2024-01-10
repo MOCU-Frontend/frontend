d46ee3faeaa865fd4034f1a200f0945b02eeddee
HS <hasang2691@naver.com>
2024-01-10 17:16:50 +0900

    Feat: mapping data

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -6,6 +6,31 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import CheckFilterSelect from '../../components/CheckFilter/Select/CheckFilterSelect';
 import StoreInfo from '../../components/SearchResult/atoms/StoreInfo/StoreInfo';
 
+type CheckFilterLabel = {
+  title: string;
+};
+
+const CheckFilterLabelData: CheckFilterLabel[] = [
+  {
+    title: '거리순',
+  },
+  {
+    title: '업종',
+  },
+  {
+    title: '이벤트',
+  },
+  {
+    title: '쿠폰 사용 임박',
+  },
+  {
+    title: '적립 진행 중만',
+  },
+  {
+    title: '안 가본 곳만',
+  },
+];
+
 type StoreData = {
   title: string;
   couponCount: number;
@@ -59,117 +84,28 @@ const SearchResult = () => {
       </div>
 
       <div className={styles.filtersWrapper}>
-        <CheckFilterSelect
-          isChecked={false}
-          label="거리순"
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
-        <CheckFilterSelect
-          isChecked={false}
-          label="업종"
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
-        <CheckFilterSelect
-          isChecked={false}
-          label="이벤트"
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
-        <CheckFilterSelect
-          isChecked={false}
-          label="쿠폰 사용 임박"
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
-        <CheckFilterSelect
-          isChecked={false}
-          label="적립 진행 중만 "
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
-        <CheckFilterSelect
-          isChecked={false}
-          label="안 가본 곳만"
-          size="small"
-          border={1}
-          borderColor="sub-purple-light"
-          borderRadius="large"
-        />
+        {CheckFilterLabelData.map((data, index) => (
+          <CheckFilterSelect
+            isChecked={false}
+            label={data.title}
+            size="small"
+            border={1}
+            borderColor="sub-purple-light"
+            borderRadius="large"
+          />
+        ))}
       </div>
 
       <div className={styles.wrapContent}>
-        <StoreInfo
-          title={searchResultData[0].title}
-          couponCount={searchResultData[0].couponCount}
-          achieve={searchResultData[0].achieve}
-          distance={searchResultData[0].distance}
-        />
-        <StoreInfo
-          title={searchResultData[1].title}
-          couponCount={searchResultData[1].couponCount}
-          achieve={searchResultData[1].achieve}
-          distance={searchResultData[1].distance}
-        />
-        <StoreInfo
-          title={searchResultData[2].title}
-          couponCount={searchResultData[2].couponCount}
-          achieve={searchResultData[2].achieve}
-          distance={searchResultData[2].distance}
-        />
-        <StoreInfo
-          title={searchResultData[3].title}
-          couponCount={searchResultData[3].couponCount}
-          achieve={searchResultData[3].achieve}
-          distance={searchResultData[3].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
-        <StoreInfo
-          title={searchResultData[4].title}
-          couponCount={searchResultData[4].couponCount}
-          achieve={searchResultData[4].achieve}
-          distance={searchResultData[4].distance}
-        />
+        {searchResultData.map((data, index) => (
+          <StoreInfo
+            key={index}
+            title={data.title}
+            couponCount={data.couponCount}
+            achieve={data.achieve}
+            distance={data.distance}
+          />
+        ))}
       </div>
     </div>
   );

98f784c8101c0ec629cc719f70bdbec1502e8fb8
unique0902 <rla0591@naver.com>
2024-01-15 17:27:28 +0900

    Feat: make etc input in locSetContent in MyLocationEdit page

--- src/components/My/Location/Edit/atoms/Contents/LocSet/MyLocationEditLocSetContent.tsx
+++ src/components/My/Location/Edit/atoms/Contents/LocSet/MyLocationEditLocSetContent.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import MyLocationEditLocSetBtn from '../../Buttons/LocSet/MyLocationEditLocSetBtn';
+import MyLocationEditDetailInput from '../../Input/Detail/MyLocationEditDetailInput';
 import MyLocationEditDetailContentTitleText from '../../Texts/ContentTitle/MyLocationEditDetailContentTitleText';
 import styles from './MyLocationEditLocSetContent.module.css';
 
 type LocSetData = {
-  name: string;
+  name: '집' | '회사' | '학교' | '기타';
   Icon: React.FC<React.SVGProps<SVGSVGElement>>;
   isChecked: boolean;
+  etcName?: string;
 };
 
 interface Props {
   locSetDataArr: LocSetData[];
   handleClickBtn: (index: number) => void;
+  handleChangeCheckedDataEtcName: (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => void;
 }
 
 const MyLocationEditLocSetContent: React.FC<Props> = ({
   locSetDataArr,
   handleClickBtn,
+  handleChangeCheckedDataEtcName,
 }: Props) => {
+  const checkedData = locSetDataArr.find((x) => x.isChecked);
+  if (!checkedData) throw new Error('no checked loc set data!');
   return (
     <div className={styles.wholeWrapper}>
       <MyLocationEditDetailContentTitleText text='주소 별칭' />
@@ -30,6 +38,12 @@ const MyLocationEditLocSetContent: React.FC<Props> = ({
           />
         ))}
       </div>
+      {checkedData.name === '기타' && (
+        <MyLocationEditDetailInput
+          value={checkedData.etcName || ''}
+          handleChange={handleChangeCheckedDataEtcName}
+        />
+      )}
     </div>
   );
 };

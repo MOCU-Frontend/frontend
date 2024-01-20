51784e11b5a383075c7a1e9ff1cde0cabfb5b8a0
HS <hasang2691@naver.com>
2024-01-20 21:46:11 +0900

    Feat: Create StoreInfoInStamp and UseCouponBtnInStamp

---
+++ src/components/Stamp/atoms/StoreInfoInStamp/StoreInfoInStamp.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styles from './StoreInfoInStamp.module.css';
+import { ReactComponent as StarImage } from '../../../../assets/icon/star.svg';
+import { ReactComponent as MapImage } from '../../../../assets/icon/mapMarkerRegularSolid.svg';
+import { colors } from '../../../../styles/colors';
+import UseCouponBtnInStamp from './UseCouponBtnInStamp/UseCouponBtnInStamp';
+
+interface Props {
+  /**
+   * 가게 이름
+   */
+  title: string;
+
+  /**
+   * 쿠폰 개수
+   */
+  couponCount: number;
+
+  /**
+   * 달성 혜택
+   */
+
+  achieve: string;
+
+  /**
+   * 거리
+   */
+  distance: number;
+  /**
+   * 클릭시 스토어 detail 페이지로 이동하게
+   */
+  onClickStoreDetailBtn: () => void;
+  /**
+   * 쿠폰 사용 버튼 클릭시 발생하는 이벤트
+   */
+  onClickCouponeBtn: () => void;
+}
+
+const StoreInfoInStamp: React.FC<Props> = ({
+  title,
+  couponCount,
+  achieve,
+  distance,
+  onClickStoreDetailBtn,
+  onClickCouponeBtn,
+}: Props) => {
+  return (
+    <button className={styles.wrapStoreInfo}>
+      <div className={styles.storeWrapImgStar}>
+        <div className={styles.storeImage} />
+        <div className={styles.storeWrapStar}>
+          <StarImage width={10} height={10} />
+          <StarImage width={10} height={10} />
+          <StarImage width={10} height={10} />
+          <StarImage width={10} height={10} />
+          <StarImage width={10} height={10} />
+        </div>
+      </div>
+
+      <div className={styles.storeWrapText}>
+        <div className={styles.storeTitle} onClick={onClickStoreDetailBtn}>
+          {title}
+        </div>
+        <div className={styles.storeWrapInfoText}>
+          <div className={styles.storeWrapCouponSentence}>
+            <div className={styles.storeCouponCount}>{couponCount}/10</div>
+            {couponCount === 10 && (
+              <UseCouponBtnInStamp onClick={onClickCouponeBtn} />
+            )}
+          </div>
+
+          <div className={styles.storeAchieve}>달성 혜택: {achieve}</div>
+        </div>
+      </div>
+
+      <div className={styles.storeWrapDistance}>
+        <MapImage width={17.6} height={22} fill={colors.greyDark00} />
+        <div className={styles.storeDistanceText}>{distance}m</div>
+      </div>
+    </button>
+  );
+};
+
+export default StoreInfoInStamp;
--- src/pages/Stamp/Stamp.tsx
+++ src/pages/Stamp/Stamp.tsx
@@ -2,17 +2,66 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Stamp.module.css';
 import HeaderBackBtn from '../../components/HeaderBackBtn/HeaderBackBtn';
+import CheckFilterSelect from '../../components/CheckFilter/Select/CheckFilterSelect';
+import StoreInfoInStamp from '../../components/Stamp/atoms/StoreInfoInStamp/StoreInfoInStamp';
+
+import {
+  searchResultData,
+  initialMenuItemDataArr,
+  MenuItemData,
+} from '../../store/data/searchResult';
 
 const Stamp = () => {
   const navigate = useNavigate();
   return (
     <div className={styles.wrapper}>
-      <HeaderBackBtn
-        onClickBackBtn={() => navigate('/home')}
-        headerTitle="적립 현황"
-      />
+      <div className={styles.headerWrapper}>
+        <HeaderBackBtn
+          onClickBackBtn={() => navigate('/home')}
+          headerTitle="적립 현황"
+        />
+      </div>
 
-      <div className={styles.wrapFilter}></div>
+      <div className={styles.filtersWrapper}>
+        <CheckFilterSelect
+          label="최신순"
+          isChecked={false}
+          border={1}
+          borderColor={'main-purple'}
+        />
+        <CheckFilterSelect
+          label="카테고리"
+          isChecked={false}
+          border={1}
+          borderColor={'main-purple'}
+        />
+        <CheckFilterSelect
+          label="이벤트"
+          isChecked={false}
+          border={1}
+          borderColor={'main-purple'}
+        />
+        <CheckFilterSelect
+          label="사용 가능 쿠폰"
+          isChecked={false}
+          border={1}
+          borderColor={'main-purple'}
+        />
+      </div>
+
+      <div className={styles.contentWrapper}>
+        {searchResultData.map((data, index) => (
+          <StoreInfoInStamp
+            key={data.title + index}
+            title={data.title}
+            couponCount={data.couponCount}
+            achieve={data.achieve}
+            distance={data.distance}
+            onClickCouponeBtn={() => {}}
+            onClickStoreDetailBtn={() => {}}
+          />
+        ))}
+      </div>
     </div>
   );
 };

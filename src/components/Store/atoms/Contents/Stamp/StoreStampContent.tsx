0baeb6ef778f0c50ebf005e527eed65b7a3698b1
unique0902 <rla0591@naver.com>
2024-01-21 22:22:02 +0900

    Feat: make StoreStampContent style

--- src/components/Store/atoms/Contents/Stamp/StoreStampContent.tsx
+++ src/components/Store/atoms/Contents/Stamp/StoreStampContent.tsx
@@ -10,20 +10,26 @@ interface Props {}
 const StoreStampContent: React.FC<Props> = ({}: Props) => {
   return (
     <div className={styles.wholeWrapper}>
-      <div className={styles.afa}>
+      <div className={styles.headerWRapper}>
         <StoreStampTitleText text='내 스탬프 적립 현황' />
         <StoreStampInfoText text='6/10' />
       </div>
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='23.11.16' />
-      <StoreStamp bodyText='' />
-      <StoreStamp bodyText='' />
-      <StoreStamp bodyText='' />
-      <StoreStamp bodyText='베이글 1개' />
+      <div className={styles.stampTable}>
+        <div className={styles.stampRow}>
+          <StoreStamp bodyText='23.11.16' />
+          <StoreStamp bodyText='23.11.16' />
+          <StoreStamp bodyText='23.11.16' />
+          <StoreStamp bodyText='23.11.16' />
+          <StoreStamp bodyText='23.11.16' />
+        </div>
+        <div className={styles.stampRow}>
+          <StoreStamp bodyText='23.11.16' />
+          <StoreStamp bodyText='' />
+          <StoreStamp bodyText='' />
+          <StoreStamp bodyText='' />
+          <StoreStamp bodyText='베이글 1개' />
+        </div>
+      </div>
     </div>
   );
 };

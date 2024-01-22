aa642fc62111711c22e1251159f5da435121b893
unique0902 <rla0591@naver.com>
2024-01-22 18:43:36 +0900

    Feat: make review basic page

---
+++ src/pages/Review/Review.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Review.module.css';
+
+interface Props {}
+
+const Review: React.FC<Props> = ({}: Props) => {
+  return <div>Review</div>;
+};
+
+export default Review;

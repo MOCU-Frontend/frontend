68de42cd58aef968c19b9099a2a799a886223cd2
unique0902 <rla0591@naver.com>
2024-01-22 20:43:37 +0900

    Feat: make InputWithCharCounter height interactive

--- src/components/TextField/atoms/InputWithCharCounter/InputWithCharCounter.tsx
+++ src/components/TextField/atoms/InputWithCharCounter/InputWithCharCounter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './InputWithCharCounter.module.css';
 interface Props {
   placeholder: string;
@@ -11,13 +11,26 @@ const InputWithCharCounter: React.FC<Props> = ({
   text,
   setText,
 }: Props) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const handleResizeHeight = () => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height =
+        textAreaRef.current.scrollHeight + 'px';
+    }
+  };
+  useEffect(() => {
+    handleResizeHeight();
+  }, [text]);
   return (
     <div className={styles.wrapper}>
       <textarea
         className={styles.textarea}
         placeholder={placeholder}
+        ref={textAreaRef}
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
       />
       <div className={styles.counterWrapper}>
         <p className={styles.counterText}>{text.length} 자</p>

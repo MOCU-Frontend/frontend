e659ed77ce91ed9811766089e5873eb16c8dfae8
unique0902 <rla0591@naver.com>
2024-01-23 14:03:49 +0900

    Feat: make SlideMenuReviewNewBodyTab in MyReviewNewContent component

--- src/components/My/Review/atoms/Content/New/MyReviewNewContent.tsx
+++ src/components/My/Review/atoms/Content/New/MyReviewNewContent.tsx
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SlideMenuReviewNewBodyTab from '../../../../../SlideMenu/atoms/BodyTab/ReviewNew/SlideMenuReviewNewBodyTab';
 import MyReviewNew from '../../New/MyReviewNew';
 import styles from './MyReviewNewContent.module.css';
 
 interface Props {}
 
+type MenuItemData = {
+  dateText: string;
+  storeCategory: string;
+  storeTitle: string;
+  accumText: string;
+  nextGift: string;
+  remainingDayNum: number;
+  isChecked: boolean;
+};
+
+const menuItemDataArr: MenuItemData[] = [
+  {
+    dateText: '11. 16. 목',
+    storeCategory: '베이커리',
+    storeTitle: '크림베이글 건대점',
+    accumText: '9/10',
+    nextGift: '블루베리 크림 베이글',
+    remainingDayNum: 4,
+    isChecked: true,
+  },
+  {
+    dateText: '11. 13. 월',
+    storeCategory: '카페',
+    storeTitle: '비에이치 테이블',
+    accumText: '2/10',
+    nextGift: '오늘의 수프',
+    remainingDayNum: 1,
+    isChecked: false,
+  },
+  {
+    dateText: '11. 14. 화',
+    storeCategory: '식당',
+    storeTitle: '파머스포케 건대점',
+    accumText: '1/10',
+    nextGift: '캔음료 1개',
+    remainingDayNum: 2,
+    isChecked: false,
+  },
+];
+
 const MyReviewNewContent: React.FC<Props> = ({}: Props) => {
+  const [reviewArr, setReviewArr] = useState<MenuItemData[]>(menuItemDataArr);
+  const handleCheckedDataIndex = (prevIndex: number, newIndex: number) => {
+    if (!reviewArr[prevIndex]) throw new Error('invalid prevIndex!!');
+    if (!reviewArr[newIndex]) throw new Error('invalid newIndex!!');
+    setReviewArr((prevArr) => {
+      const copiedArr = [...prevArr];
+      copiedArr[prevIndex].isChecked = false;
+      copiedArr[newIndex].isChecked = true;
+      return copiedArr;
+    });
+  };
   return (
     <div className={styles.myReviewNewsWrapper}>
-      <div className={styles.myReviewNewWrapper}>
-        <MyReviewNew
-          dateText='11. 16. 목'
-          storeCategory='베이커리'
-          storeTitle='크림베이글 건대점'
-          accumText='9/10'
-          nextGift='블루베리 크림 베이글'
-          remainingDayNum={4}
-        />
-      </div>
+      <SlideMenuReviewNewBodyTab
+        menuItemDataArr={reviewArr}
+        handleCheckedDataIndex={handleCheckedDataIndex}
+      />
     </div>
   );
 };

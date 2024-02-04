7ac7a6279b7b3917f96f03c7359ab15f698d0653
unique0902 <rla0591@naver.com>
2024-02-05 01:47:03 +0900

    Feat: add handleAddSeachKeyword and make useRecentSearchWord hook and if searchWord value exist in StoreSearch page

--- src/pages/Store/Search/StoreSearch.tsx
+++ src/pages/Store/Search/StoreSearch.tsx
@@ -7,24 +7,17 @@ import StoreSearchKeyword from '../../../components/StoreSearch/atoms/StoreSearc
 import StoreSearchRecent from '../../../components/StoreSearch/atoms/StoreSearchRecent/StoreSearchRecent';
 import StoreSearchImminentCoupon from '../../../components/StoreSearch/atoms/StoreSearchImminentCoupon/StoreSearchImminentCoupon';
 import StoreSearchRecommend from '../../../components/StoreSearch/atoms/StoreSearchRecommend/StoreSearchRecommend';
-import { storeRecentSearchData } from '../../../store/data/storeSearch';
+import { useRecentSearchWord } from '../../../hooks/useRecentSearchWord';
 
 const StoreSearch = () => {
-  const [searchKeywordDataArr, setSearchKeywordDataArr] = useState(
-    storeRecentSearchData
-  );
-  const handleDeleteSeachKeyword = (index: number) => {
-    if (!searchKeywordDataArr[index])
-      throw new Error('invalid index seachKeyword!');
-    setSearchKeywordDataArr((arr) => {
-      const arrCopied = [...arr];
-      arrCopied.splice(index, 1);
-      return arrCopied;
-    });
-  };
+  const {
+    searchKeywordDataArr,
+    handleDeleteSeachKeyword,
+    handleAddSeachKeyword,
+  } = useRecentSearchWord();
   return (
     <div className={styles.wrapper}>
-      <StoreSearchHeader />
+      <StoreSearchHeader handleAddSeachKeyword={handleAddSeachKeyword} />
       <div className={styles.contentWrapper}>
         <StoreSearchKeyword
           searchKeywordDataArr={searchKeywordDataArr}
